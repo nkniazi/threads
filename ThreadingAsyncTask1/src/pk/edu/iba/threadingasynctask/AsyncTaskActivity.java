1d8eb4845c9bdbc9ae5d4a32b07a1472cd8eb0c4
nkniazi <alnasrkhan@gmail.com>
2014-10-14 08:47:51 +0500

    Initial Nasr

---
+++ ThreadingAsyncTask1/src/pk/edu/iba/threadingasynctask/AsyncTaskActivity.java
@@ -0,0 +1,15 @@
+package pk.edu.iba.threadingasynctask;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class AsyncTaskActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.async_task);
+	}
+}
